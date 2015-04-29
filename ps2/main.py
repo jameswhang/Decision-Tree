0921ee7e0eafdbec31211327b5ea7bcc07e1cace
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-04-29 03:51:50 -0500

    parsing input file into a dict

--- ps2/main.py
+++ ps2/main.py
@@ -9,6 +9,7 @@ import sys
 
 # main function
 def main():
+    allDataSet = []
     if len(sys.argv) < 3:
         WrongUsage()
         sys.exit()
@@ -16,7 +17,32 @@ def main():
         WrongUsage()
     else:
         inputFile = sys.argv[2]
-        inputFileContent = open(inputFile).read()
+        inputFileContent = open(inputFile)
+        firstLine = True
+        for line in inputFileContent:
+            if firstLine:
+                firstLine = False
+                continue
+            line = line.replace('\n', '')
+            newData = line.split(',')
+            newDict = {
+                'winPercentageA': newData[0],
+                'winPercentageB': newData[1],
+                'weather': newData[2],
+                'temperature': newData[3],
+                'numInjuriesA': newData[4],
+                'numInjuriesB': newData[5],
+                'startPitcherA': newData[6],
+                'startPitcherB': newData[7],
+                'daysSinceLastGameA': newData[8],
+                'daysSinceLastGameB': newData[9],
+                'homeForA': newData[10],
+                'runDifferentialA': newData[11],
+                'runDifferentialB': newData[12],
+                'winner': newData[13]
+            }
+            allDataSet.append(newDict)
+    print allDataSet[0]
 
 
 # wrong usage
