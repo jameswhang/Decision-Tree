2191cc7f33608a9b5e99c47d1d15a348949d90fb
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-05-02 04:55:00 -0500

    refactored main

--- ps2/main.py
+++ ps2/main.py
@@ -13,109 +13,96 @@ import pickle
 
 # main function
 def main():
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         WrongUsage()
         sys.exit()
-    elif sys.argv[1] != '-t':
-        WrongUsage()
     else:
-        inputFile = sys.argv[2]
-        inputFileContent = open(inputFile)
-        firstLine = True
-        attributes = {}
-        trainData = []
-        valueList = []
-        attr_keys = []
-        for line in inputFileContent:
-            if firstLine:
-                attrs = line.split(',')
-                for attr in attrs:
-                    if attr == attrs[len(attrs) - 1]:
-                        break
-                    attr = attr.replace('\n', '').replace(' ', '')
-                    attrType = raw_input('Found an attr, ' + attr +
-                                         '. Type? [c: continuous (numeral), ' +
-                                         'd: discrete(nominal)] : ')
-                    attr = attr.replace(' ', '')
-                    attributes[attr] = attrType.replace('\n', '')
-                    attr_keys.append(attr)
-                firstLine = False
-                continue
-            line = line.replace('\n', '')
-            newData = line.split(',')
-            i = 0
-            newDict = {}
+        trainFile = sys.argv[1]
+        printIntro()
+        readResult = readDataFromFile(trainFile)
+        trainData = readResult[0]
+        attrDict = readResult[1]
+        targetLabelList = readResult[2]
 
-            doAppend = True
-            for data in newData:
-                if i == (len(newData) - 1):
-                    if newData[i] == '?':
-                        doAppend = False
-                    else:
-                        valueList.append(int(newData[i]))
+        attributeList = attrDict.keys()
+
+        tTree = train.GenerateDTree(trainData, attributeList, attrDict, targetLabelList)
+
+        saveTree = raw_input('Finished generating the tree! Would you like to save it? [y/n]    ').replace('\n', '')
+        if saveTree == 'y':
+            how = raw_input('How should I save the tree?\n [p: Python object form    t: text file').replace('\n', '')
+            if how == 'p':
+                with open('tree.pkl', 'wb') as output:
+                    pickle.dump(tTree, output, pickle.HIGHEST_PROTOCOL)
+            elif how == 't':
+                oStream = raw_input('Enter the name of the result file:        ')
+                tree.saveTree(oStream)
+
+        validate = raw_input('Do you want to validate the generated tree? [y/n]        ').replace('\n', '')
+        if validate == 'y':
+            how = raw_input('How would you like to validate it?\n[1: n-fold cross-validation    ' +
+                            '2: with a validation file        ')
+            if how == '1':
+                N = int(raw_input('What is N?    '))
+                validate.nFold(trainData, valueList, attributes, N)
+            else:
+                vFilepath = raw_input('Where is it?        ').replace('\n', '')
+                vFileRead = readDataFromFile(vFilepath, 'rb')
+                trainData = vFileRead[0]
+                trainLabelList = vFileRead[2]
+                validate.validate(tree, vData, trainLabelList)
+        print "Bye!"
+
+# readInput
+def readDataFromFile(filepath):
+    inputFile = sys.argv[1]
+    inputFileContent = open(inputFile)
+    firstLine = True
+    attrDict = {}
+    dataList = []
+    targetLabelList = []
+    attr_keys = []
+    for line in inputFileContent:
+        if firstLine:
+            attrs = line.split(',')
+            for attr in attrs:
+                if attr == attrs[len(attrs) - 1]:
                     break
+                attr = attr.replace('\n', '').replace(' ', '')
+                while True:
+                    attrType = raw_input('Found an attr, ' + attr +
+                                        '. Type? [c: continuous (numeral), ' +
+                                        'd: discrete(nominal)] : ')
+                    attrType = attrType.replace('\n', '')
+                    if attrType == 'c' or attrType == 'd':
+                        break
+                    else:
+                        print "Wrong entry. Only type c or d"
+                attrDict[attr] = attrType
+                attr_keys.append(attr)
+            firstLine = False
+            continue
+        line = line.replace('\n', '')
+        newData = line.split(',')
+        i = 0
+        newDict = {}
+        doAppend = True
+        for data in newData:
+            if i == (len(newData) - 1):
                 if newData[i] == '?':
-                    newDict[attr_keys[i]] = newData[i]
+                    doAppend = False
                 else:
-                    newDict[attr_keys[i]] = float(newData[i])
-                i += 1
-            if doAppend:
-                # print newDict
-                trainData.append(newDict)
-    trainData = preprocessData(trainData, attributes)
-    attributeList = attributes.keys()
-
-    tree = train.GenerateDTree(trainData, attributeList, attributes, valueList)
-    
-    saveTree = raw_input('Tree is generated! Would you like to save it? [y/n]').replace('\n', '')
-    if saveTree == 'y':
-        how = raw_input('How should I save the tree? [p: pickle (Python object form) t: text file').replace('\n', '')
-        if how == 'p':
-            with open('tree.pkl', 'wb') as output:
-                pickle.dump(tree, output, pickle.HIGHEST_PROTOCOL)
-        elif how == 't':
-            oStream = raw_input('Enter the name of the result file: ')
-            tree.saveTree(oStream)
-    
-    validate = raw_input('Do you want to validate it? [y/n]').replace('\n', '')
-    if validate == 'y':
-        how = raw_input('How would you like to validate it? [1: n-fold cross validation  2: with a validation set').replace('\n', '')
-        if how == '1':
-            N = int(raw_input('What is N?  '))
-            validate.nFold(trainData, valueList, attributes, N)
-        else:
-            vFilepath = raw_input('Where is it?').replace('\n', '')
-            vContent = open(vFilepath, 'rb')
-            vData = []
-            vValuelist = []
-            firstLine = True
-            for line in vContent:
-                if firstLine:
-                    pass
-                else:
-                    line = line.replace('\n', '')
-                    newData = line.split(',')
-                    i = 0
-                    newDict = {}
-
-                    doAppend = True
-                    for data in vData:
-                        if i == (len(vData) - 1):
-                            if vData[i] == '?':
-                                doAppend = False
-                            else:
-                                vValuelist.append(int(vData[i]))
-                            break
-                        if vData[i] == '?':
-                            newDict[attr_keys[i]] = vData[i]
-                        else:
-                            newDict[attr_keys[i]] = float(vData[i])
-                        i += 1
-                    if doAppend:
-                        # print newDict
-                        vData.append(newDict)
-            validate.validate(tree, vData, vValuelist)
-    print 'BYE!'
+                    targetLabelList.append(int(newData[i]))
+                break
+            if newData[i] == '?':
+                newDict[attr_keys[i]] = newData[i]
+            else:
+                newDict[attr_keys[i]] = float(newData[i])
+            i += 1
+        if doAppend:
+            dataList.append(newDict)
+    dataList = preprocessData(dataList, attrDict)
+    return [dataList, attrDict, targetLabelList]
 
 
 # preProcess
@@ -123,7 +110,9 @@ def preprocessData(allDataSet, attrDict):
     newDataSet = []
     averages = {}
     mcvs = {}
+    #print allDataSet
     for entry in allDataSet:
+        #print entry
         entry = entry.copy()
         for key in entry.keys():
             if entry[key] == '?':
@@ -169,6 +158,7 @@ def mostCommonValue(allDataSet, key):
     #print maxCount
     return mcv
 
+
 def findAverage(allDataSet, key):
     sumValue = 0
     unknowns = 0
@@ -187,6 +177,12 @@ def findAverage(allDataSet, key):
 def WrongUsage():
     print "Usage: ./ps2 -t [training_file_path] -v [validate_file_path] "
 
+def printIntro():
+    print "########################################"
+    print "#      EECS 349 : Machine Learning     #"
+    print "#       Written by : James Whang       #"
+    print "#     Decision Tree Implementation     #"
+    print "########################################"
 
 if __name__ == '__main__':
     main()
